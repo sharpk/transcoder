890c49cab7c527b791742370873ee24c0c1c63b0
Ken Sharp <sharpk60@yahoo.com>
2013-03-06 11:34:02 -0600

    Add proper logging
    also fix minor logic error in IsNPVRBusy()

--- transcode_daemon.py
+++ transcode_daemon.py
@@ -7,15 +7,16 @@ import re
 import shutil
 import sys
 import subprocess
+import logging
 
 # Config
-debugFlag = False
+loglevel = logging.DEBUG
 handbrakePath = "C:\\Program Files\\Handbrake\\"
 btDownloadPath = "C:\\Documents and Settings\\Ken\\My Documents\\Downloads"
 btbtInputFileExt = '.*\.avi$|.*\.mkv$|.*\.mp4$|.*\.3gp$'
 npvrEnable = True
 npvrPath = "C:\\Program Files\NPVR\\"
-npvrRecordingPath = ""
+npvrRecordingPath = "C:\\Documents and Settings\\All Users\\Documents\\My Videos"
 destinationBasePath = "C:\\media\\video"
 outputFileExt = ".m4v"
 replacementChar = " "
@@ -33,53 +34,57 @@ def CalcDestinationPath(prettyFileBaseName):
 		splitArr = re.split(pattern, prettyFileBaseName)
 		if len(splitArr) > 1:
 			resultingFolderName = string.strip(splitArr[0])
-			print "Found folder name: " + resultingFolderName
+			logging.debug("Found TV folder name: " + resultingFolderName)
 			destinationFilePath = os.path.join( destinationBasePath, resultingFolderName)
 			if not os.path.isdir(destinationFilePath):
 				# TODO make sure that case is ignored when looking for matching directory
 				os.mkdir(destinationFilePath)
-				print "Made new directory: " + destinationFilePath
+				logging.debug("Made new directory: " + destinationFilePath)
 			destinationFilePath = os.path.join( destinationFilePath, prettyFileBaseName + outputFileExt)
 			tvFound = True
 			break
 	if not tvFound:
 		destinationFilePath = os.path.join( destinationBasePath, prettyFileBaseName + outputFileExt)
-		print "Could not calculate folder name"
+		logging.debug("Could not calculate TV folder name")
 	return destinationFilePath
 
+#returns True if successful, False if there was an error
 def ConvertVideoFile(sourceFilePath, destinationFilePath):
 	# don't start Handbrake if NPVR is recording
 	if IsNPVRBusy():
-		return
+		return True
 	# Call Handbrake
 	handbrakeCmdLine = "HandbrakeCLI.exe -i \"" + sourceFilePath + "\" -o \"" + destinationFilePath + "\" --preset=\"Normal\" > hb.log"
-	print "Handbrake Command Line: " + handbrakeCmdLine
+	logging.debug("Handbrake Command Line: " + handbrakeCmdLine)
 	os.chdir(handbrakePath)
-	if debugFlag:
+	if loglevel == logging.DEBUG:
 		shutil.copy(sourceFilePath, destinationFilePath)
 	else:
 		os.system(handbrakeCmdLine)
 	# cleanup source file
 	if os.path.exists(destinationFilePath):
-		if debugFlag:
-			print "Not deleting source file since debugFlag = true"
+		if loglevel == logging.DEBUG:
+			logging.debug("Not deleting source file since loglevel == logging.DEBUG")
+            return True
 		else:
-			print "Deleting source file"
+			logging.debug("Deleting source file")
 			os.remove(sourceFilePath)
+            return True
 	else:
-		print "Error: file " + destinationFilePath + " does not exist. Handbrake probably failed."
+		logging.error("Error: file " + destinationFilePath + " does not exist. Handbrake probably failed.")
+        return False
 	
 def ScanForBtFiles():
 	os.chdir(btDownloadPath)	
 	for f in os.listdir('.'):
 		if re.match(btInputFileExt, f):
 			sourceFilePath = os.path.join(btDownloadPath, f)
-			print "Found a file: " + sourceFilePath
+			logging.debug("Found a file: " + sourceFilePath)
 			# replace dots so it appears correctly on Roku
 			prettyFileBaseName = btMakePrettyFileName(sourceFilePath)
 			# if it's a TV episode then calculate a show folder, create the folder if necessary, and calc final path
 			destinationFilePath = CalcDestinationPath(prettyFileBaseName)
-			print "Prettified destination path: " + destinationFilePath
+			logging.debug("Prettified destination path: " + destinationFilePath)
 			# Use Handbrake to do the transcode
 			ConvertVideoFile(sourceFilePath, destinationFilePath)
 
@@ -89,7 +94,8 @@ def IsNPVRBusy():
 	os.chdir(npvrPath)
 	p = subprocess.Popen('NScriptHelper.exe -isinuse', shell=True, stdout=subprocess.PIPE)
 	retVal = p.stdout.read()
-	if (retVal.find('NOT RECORDING') > 0:
+    logging.debug("NScriptHelper -isinuse output: " + retVal)
+	if retVal.find('NOT RECORDING') >= 0:
 		return False
 	else:
 		return True
@@ -101,7 +107,7 @@ def ScanForNPVRFiles():
 	for f in os.listdir('.'):
 		if re.match('.*\.done$', f):
 			doneFilePath = os.path.join(npvrRecordingPath, f)
-			print "Found a file: " + sourceFilePath
+			logging.debug("Found a file: " + sourceFilePath)
 			# get rid of *.done to get actual movie file path
 			sourceFilePath = os.path.join(npvrRecordingPath, os.path.splitext(f)[0])
 			# run comskip and comclean(2|3) to remove commercials
@@ -113,20 +119,20 @@ def ScanForNPVRFiles():
 def SanityCheck():
 	# check that directories exist
 	if not os.path.exists(handbrakePath):
-		print "Error: Handbrake path does not exist: " + handbrakePath
+		logging.error("Error: Handbrake path does not exist: " + handbrakePath)
 		return False
 	if not os.path.exists(btDownloadPath):
-		print "Error: bittorrent download path does not exist: " + btDownloadPath
+		logging.error("Error: bittorrent download path does not exist: " + btDownloadPath)
 		return False
 	if not os.path.exists(destinationBasePath):
-		print "Error: Destination path does not exist: " + destinationBasePath
+		logging.error("Error: Destination path does not exist: " + destinationBasePath)
 		return False
 	if npvrEnable:
 		if not os.path.exists(npvrPath):
-			print "Error: NextPVR path does not exist: " + npvrPath
+			logging.error("Error: NextPVR path does not exist: " + npvrPath)
 			return False
 		if not os.path.exists(npvrRecordingPath):
-			print "Error: NextPVR recording path does not exist: " + npvrRecordingPath
+			logging.error("Error: NextPVR recording path does not exist: " + npvrRecordingPath)
 			return False
 		
 	# check that PostProcessing.bat is installed and contains code to create *.done files
@@ -137,6 +143,8 @@ def SanityCheck():
 
 if __name__ == "__main__":
 	print "Starting transcode daemon, hit Ctrl-C to exit"
+    logging.basicConfig(filename='transcode_daemon.log',level=logging.DEBUG)
+    logging.debug('Starting trasncode daemon')
 	sane = SanityCheck()
 	if not sane:
 		sys.exit()
